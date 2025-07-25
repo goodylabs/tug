0e2e00f241b3965ea8e7fc6b3a81623aaf841c57
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-07-25 11:41:06 +0200

    dir refactor

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,41 +4,47 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-
-	"github.com/joho/godotenv"
 )
 
 var (
 	BASE_DIR   string
 	DEVOPS_DIR string
+	TUG_ENV    string
 )
 
-func init() {
-	godotenv.Load(".env")
-	BASE_DIR = findProjectRoot()
-	DEVOPS_DIR = getEnvOrDefault("DEVOPS_DIR", "devops")
+func LoadProductionConfig() {
+	BASE_DIR = getEnvOrError("PWD")
+	DEVOPS_DIR = "devops"
 }
 
-func getEnvOrDefault(envName string, defaultValue string) string {
+func LoadDevelopmentConfig() {
+	BASE_DIR = filepath.Join(findProjectRoot(), ".development")
+	DEVOPS_DIR = "devops"
+}
+
+func LoadTestingConfig() {
+	BASE_DIR = filepath.Join(findProjectRoot(), ".testing")
+	DEVOPS_DIR = "devops"
+}
+
+func getEnvOrError(envName string) string {
 	value := os.Getenv(envName)
 	if value == "" {
-		return defaultValue
+		log.Fatalf("Environment variable %s is not set - tug does not support your shell configuration...", envName)
 	}
 	return value
 }
 
 func findProjectRoot() string {
-	dir := getEnvOrDefault("BASE_DIR", "")
-	if dir != "" {
-		abs, err := filepath.Abs(dir)
-		if err != nil {
-			log.Fatal(err)
+	dir, _ := os.Getwd()
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			return dir
 		}
-		return abs
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			log.Fatal("Could not find project root with go.mod file")
+		}
+		dir = parent
 	}
-	abs, err := filepath.Abs(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return abs
 }
