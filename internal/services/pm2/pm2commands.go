40783a87a16f4c2ff26035d034c79a62364fb6f5
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-07-28 13:11:34 +0200

    select resource

--- internal/services/pm2/pm2commands.go
+++ internal/services/pm2/pm2commands.go
@@ -8,23 +8,31 @@ import (
 )
 
 const (
-	JLIST_CMD = `pm2 jlist | sed -n '/^\[/,$p'`
+	JLIST_CMD = `source ~/.nvm/nvm.sh; pm2 jlist | sed -n '/^\[/,$p'`
 )
-
-func (p *Pm2Manager) jsonOutputHandler(output string, dtoStruct any) error {
-	return json.Unmarshal([]byte(output), &dtoStruct)
-}
 
 func (p *Pm2Manager) SelectResource() string {
 	output, err := p.sshConnector.RunCommand(JLIST_CMD)
+
+	log.Println("Raw PM2 output:")
+	log.Println(output)
+
 	if err != nil {
 		log.Fatalf("Error running PM2 command: %v", err)
 	}
 
 	var pm2List []dto.Pm2ListItemDTO
-	err = p.jsonOutputHandler(output, &pm2List)
-	if err != nil {
-		log.Fatalf("Error parsing PM2 list output: %v", err)
+	if err := json.Unmarshal([]byte(output), &pm2List); err != nil {
+		log.Fatalf("Error parsing PM2 list output: %v after running `%s`", err, JLIST_CMD)
 	}
-	return pm2List[0].Name
+
+	pm2Resources := make([]string, len(pm2List))
+	for i, item := range pm2List {
+		pm2Resources[i] = item.Name
+	}
+	if len(pm2Resources) == 0 {
+		log.Fatal("No PM2 resources found")
+	}
+
+	return p.prompter.ChooseFromList(pm2Resources, "Select PM2 resource")
 }
