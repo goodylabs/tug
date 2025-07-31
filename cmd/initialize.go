c4a0d85fb287b0563f8450ea5ff79e90f7a82fa8
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-07-31 14:30:21 +0200

    update

--- cmd/initialize.go
+++ cmd/initialize.go
@@ -13,12 +13,7 @@ import (
 var initializeCmd = &cobra.Command{
 	Use:   "initialize",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Initialize configuration for tug`,
 	Run: func(cmd *cobra.Command, args []string) {
 		container := internal.InitDependencyContainer("")
 		err := container.Invoke(func(pm2UseCase *application.InitializeUseCase) error {
--- cmd/update.go
+++ cmd/update.go
@@ -14,13 +14,8 @@ import (
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
 	Use:   "update",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Update tug to the latest version",
+	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
 		shell := os.Getenv("SHELL")
 		if shell == "" {
