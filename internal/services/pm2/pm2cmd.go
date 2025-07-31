b19c426d08eced57dfe7a73462f20ca4d3e4510b
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-07-31 12:44:02 +0200

    errors communiation

--- internal/services/pm2/pm2cmd.go
+++ internal/services/pm2/pm2cmd.go
@@ -14,18 +14,15 @@ const (
 	restartCmdTemplate  = `source ~/.nvm/nvm.sh; pm2 restart %s`
 	describeCmdTemplate = `source ~/.nvm/nvm.sh; pm2 describe %s && read`
 	monitCmdTemplate    = `source ~/.nvm/nvm.sh; pm2 monit %s`
-	bashCmdTemplate     = `bash`
-	topCmdTemplate      = `top`
 )
 
 var CommandTemplates = map[string]string{
-	"pm2 logs <resource>":     logsCmdTemplate,
-	"pm2 show <resource>":     showCmdTemplate,
-	"pm2 restart <resource>":  restartCmdTemplate,
-	"pm2 describe <resource>": describeCmdTemplate,
-	"pm2 monit <resource>":    monitCmdTemplate,
-	"bash":                    bashCmdTemplate,
-	"top":                     topCmdTemplate,
+	"[pm2] logs <resource>":     logsCmdTemplate,
+	"[pm2] show <resource>":     showCmdTemplate,
+	"[pm2] restart <resource>":  restartCmdTemplate,
+	"[pm2] describe <resource>": describeCmdTemplate,
+	"[pm2] monit <resource>":    monitCmdTemplate,
+	// "[shell] bash":              bashCmdTemplate,
 }
 
 func (p *Pm2Manager) GetAvailableResources(sshConfig *dto.SSHConfig) ([]string, error) {
