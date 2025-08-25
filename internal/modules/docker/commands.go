ec61fd3d590aa52c47ff2cd81a288008cef96028
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-08-25 14:17:04 +0200

    docker cmd

--- internal/modules/docker/commands.go
+++ internal/modules/docker/commands.go
@@ -2,15 +2,18 @@ package docker
 
 const dockerListCmd = "docker ps --format json"
 
+const continueMsg = "echo 'Done, press Enter to continue...' && read"
+
 var commandTemplates = map[string]string{
-	"[docker]  logs <resource> -f":             "docker logs -f %s && read",
+	"[docker]  logs <resource> -f":             "docker logs -f %s",
 	"[docker]  exec -u root -ti <resource> sh": "docker exec -u root -it %s sh",
-	"[docker]  logs <resource>":                "docker logs %s && read",
-	"[docker]  restart <resource>":             "docker restart %s && read",
+	"[docker]  logs <resource>":                "docker logs %s && " + continueMsg,
+	"[docker]  restart <resource>":             "docker restart %s && " + continueMsg,
+	"[docker]  stats":                          "docker stats",
 	"[docker]  ps":                             "watch docker ps %s",
 	"[docker]  ps -a":                          "watch docker ps -a %s",
 	"[bash]    bash":                           "bash",
 	"[bash]    htop":                           "htop",
 	"[django]  python manage.py shell":         "docker exec -u root -it %s python manage.py shell",
-	"[traefik] show reverse-proxy config":      "docker exec %s sh -c 'apk add --no-cache --no-progress -q curl && curl localhost:8080/api/rawdata' | jq && read",
+	"[traefik] show reverse-proxy config":      "docker exec %s sh -c 'apk add --no-cache --no-progress -q curl && curl localhost:8080/api/rawdata' | jq && " + continueMsg,
 }
