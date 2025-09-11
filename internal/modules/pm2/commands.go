4b70aac894ef0080a59676373a94df388745ba19
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2025-09-11 13:25:01 +0200

    pm2 logs cmd

--- internal/modules/pm2/commands.go
+++ internal/modules/pm2/commands.go
@@ -1,26 +1,18 @@
 package pm2
 
 const (
-	jlistCmd            = `source ~/.nvm/nvm.sh; pm2 jlist | sed -n '/^\[/,$p'`
-	logsCmdTemplate     = `source ~/.nvm/nvm.sh; pm2 logs %s | less`
-	logsAllCmdTemplate  = `source ~/.nvm/nvm.sh; pm2 logs`
-	showCmdTemplate     = `source ~/.nvm/nvm.sh; pm2 show %s && read`
-	restartCmdTemplate  = `source ~/.nvm/nvm.sh; pm2 restart %s`
-	describeCmdTemplate = `source ~/.nvm/nvm.sh; pm2 describe %s && read`
-	monitCmdTemplate    = `source ~/.nvm/nvm.sh; pm2 monit`
-	updateCmdTemplate   = `source ~/.nvm/nvm.sh; pm2 update`
-	bashCmdTemplate     = `source ~/.nvm/nvm.sh; bash`
-	htopCmdTemplate     = `source ~/.nvm/nvm.sh; htop`
+	jlistCmd = `source ~/.nvm/nvm.sh; pm2 jlist | sed -n '/^\[/,$p'`
 )
 
 var commandTemplates = map[string]string{
-	"[pm2]  logs     <resource> | less": logsCmdTemplate,
-	"[pm2]  logs":                       logsAllCmdTemplate,
-	"[pm2]  show     <resource>":        showCmdTemplate,
-	"[pm2]  restart  <resource>":        restartCmdTemplate,
-	"[pm2]  describe <resource>":        describeCmdTemplate,
-	"[pm2]  monit":                      monitCmdTemplate,
-	"[pm2]  update":                     updateCmdTemplate,
-	"[bash] bash":                       bashCmdTemplate,
-	"[bash] htop":                       htopCmdTemplate,
+	"[pm2]  logs     <resource> | less": `source ~/.nvm/nvm.sh; pm2 logs %s | less`,
+	"[pm2]  logs     <resource>":        `source ~/.nvm/nvm.sh; pm2 logs %s`,
+	"[pm2]  logs":                       `source ~/.nvm/nvm.sh; pm2 logs`,
+	"[pm2]  show     <resource>":        `source ~/.nvm/nvm.sh; pm2 show %s && read`,
+	"[pm2]  restart  <resource>":        `source ~/.nvm/nvm.sh; pm2 restart %s`,
+	"[pm2]  describe <resource>":        `source ~/.nvm/nvm.sh; pm2 describe %s && read`,
+	"[pm2]  monit":                      `source ~/.nvm/nvm.sh; pm2 monit`,
+	"[pm2]  update":                     `source ~/.nvm/nvm.sh; pm2 update`,
+	"[bash] bash":                       `source ~/.nvm/nvm.sh; bash`,
+	"[bash] htop":                       `source ~/.nvm/nvm.sh; htop`,
 }
