b4e4daeae9b6b75d31831e20386d18b1755423a7
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2026-02-24 14:31:35 +0100

    pm2 host

--- cmd/pm2.go
+++ cmd/pm2.go
@@ -11,19 +11,25 @@ var pm2Cmd = &cobra.Command{
 	Use:   "pm2",
 	Short: "Abstraction layer for pm2 operations related to project repo",
 	Run: func(cmd *cobra.Command, args []string) {
+		checkConnectionUseCase := app.NewCheckConnectionUseCase()
+		useModuleUseCase := app.NewUseModuleV2UseCase()
 
 		if check, _ := cmd.Flags().GetBool("check"); check == true {
-			checkConnectionUseCase := app.NewCheckConnectionUseCase()
 			if err := checkConnectionUseCase.Execute(loadproject.Pm2Strategy); err != nil {
 				cmd.PrintErrf("%v\n", err)
 			}
 			return
 		}
 
-		useCase := app.NewUseModuleV2UseCase()
-		err := useCase.Execute(loadproject.Pm2Strategy, action.Pm2)
+		if host, _ := cmd.Flags().GetString("host"); host != "" {
+			user, _ := cmd.Flags().GetString("user")
+			if err := useModuleUseCase.ExecuteDirect(user, host, action.Pm2); err != nil {
+				cmd.PrintErrf("%v\n", err)
+			}
+			return
+		}
 
-		if err != nil {
+		if err := useModuleUseCase.Execute(loadproject.Pm2Strategy, action.Pm2); err != nil {
 			cmd.PrintErrf("%v\n", err)
 		}
 
@@ -33,4 +39,6 @@ var pm2Cmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(pm2Cmd)
 	pm2Cmd.Flags().Bool("check", false, "Check SSH connections before running Docker commands")
+	pm2Cmd.Flags().String("host", "", "Manually input host - won't use project config")
+	pm2Cmd.Flags().String("user", "root", "Manually input user - won't use project config")
 }
