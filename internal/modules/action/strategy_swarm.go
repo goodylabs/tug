4672c25ad006ad93dd49a5821bf956214c3daea9
Franciszek Szeptycki <franciszek.szeptycki@goodylabs.com>
2026-03-03 09:19:03 +0100

    swarm cmd typo

--- internal/modules/action/strategy_swarm.go
+++ internal/modules/action/strategy_swarm.go
@@ -18,18 +18,18 @@ func (s *SwarmActionStrategy) GetTemplates() map[string]string {
 	const continueMsg = "echo 'Done, press Enter to continue...' && read"
 
 	return map[string]string{
-		".host  - bash":                     "bash",
-		".host  - htop":                     "htop",
-		"swarm - ls (watch)":                "watch docker service ls",
-		"swarm - ps <xyz> (watch)":          "watch docker service ps %s --no-trunc",
-		"swarm - ps <xyz> running (watch)":  `watch 'docker service ps --filter desired-state=running --format "{{.ID}} {{.Name}} - {{.Node}} | {{.Image}}" %s'`,
-		"swarm - inspect <xyz> | jq | less": "docker service inspect %s | jq | less",
-		"swarm - restart <xyz>":             "docker service update %s --force && " + continueMsg,
-		"swarm - logs -f <xyz>":             "docker service logs -f %s",
-		"swarm - logs <xyz> | less":         "docker service logs %s | less",
-		"swarm - scale <xyz> to 0":          "docker service scale %s=0 && " + continueMsg,
-		"swarm - scale <xyz> to 1":          "docker service scale %s=1 && " + continueMsg,
-		"swarm - scale <xyz> to 3":          "docker service scale %s=3 && " + continueMsg,
+		".host  - bash":                             "bash",
+		".host  - htop":                             "htop",
+		"swarm - ls (watch)":                        "watch docker service ls",
+		"swarm - ps <xyz> (watch)":                  "watch docker service ps %s --no-trunc",
+		"swarm - ps <xyz> [ only running ] (watch)": `watch 'docker service ps --filter desired-state=running --format "{{.ID}} {{.Name}} - {{.Node}} | {{.Image}}" %s'`,
+		"swarm - inspect <xyz> | jq | less":         "docker service inspect %s | jq | less",
+		"swarm - restart <xyz>":                     "docker service update %s --force && " + continueMsg,
+		"swarm - logs -f <xyz>":                     "docker service logs -f %s",
+		"swarm - logs <xyz> | less":                 "docker service logs %s | less",
+		"swarm - scale <xyz> to 0":                  "docker service scale %s=0 && " + continueMsg,
+		"swarm - scale <xyz> to 1":                  "docker service scale %s=1 && " + continueMsg,
+		"swarm - scale <xyz> to 3":                  "docker service scale %s=3 && " + continueMsg,
 	}
 }
 
